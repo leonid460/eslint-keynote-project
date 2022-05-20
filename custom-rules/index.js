ee4a8c3faed6ae98686d7239ac2806b9051c2238
leonid460 <leonid.kol4in@gmail.com>
2022-05-20 13:26:40 +0300

    add fixer to rule

--- custom-rules/index.js
+++ custom-rules/index.js
@@ -1,11 +1,15 @@
 module.exports = {
   rules: {
     'no-distinct-from-local-styled': {
+      meta: {
+        fixable: 'code'
+      },
       create: function (context) {
         return {
           ImportDeclaration(node) {
             const { specifiers, source } = node;
             const message = `import from local styled only under "Styled" namespace`;
+            const fix = (fixer) => fixer.replaceText(node, `import * as Styled from './styled'`);
 
             if (source.value !== './styled') {
               return;
@@ -14,14 +18,14 @@ module.exports = {
             specifiers.forEach(({ type, local }) => {
               if (type === 'ImportNamespaceSpecifier') {
                 if (local.name !== 'Styled') {
-                  context.report({ node, message });
+                  context.report({ node, message, fix });
                 }
 
                 return;
               }
 
               if (type === 'ImportSpecifier' || type === 'ImportDefaultSpecifier') {
-                context.report({ node, message });
+                context.report({ node, message, fix });
               }
             });
           }
