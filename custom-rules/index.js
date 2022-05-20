a83924d91d8cae22ecb6ed370f75de567bf70c70
leonid460 <leonid.kol4in@gmail.com>
2022-05-20 13:36:54 +0300

    add args to rule

--- custom-rules/index.js
+++ custom-rules/index.js
@@ -5,27 +5,32 @@ module.exports = {
         fixable: 'code'
       },
       create: function (context) {
+        const { options } = context;
+        const { styledFileName , namespaceName } = options[0];
+        const message = `import from local ${styledFileName} only under "${namespaceName}" namespace`;
+
         return {
           ImportDeclaration(node) {
             const { specifiers, source } = node;
-            const message = `import from local styled only under "Styled" namespace`;
-            const fix = (fixer) => fixer.replaceText(node, `import * as Styled from './styled'`);
 
-            if (source.value !== './styled') {
+            const fix = (fixer) => fixer.replaceText(node, `import * as Styled from './styled'`);
+            const errorReport = () => context.report({ node, message, fix });
+
+            if (source.value !== `./${styledFileName}`) {
               return;
             }
 
             specifiers.forEach(({ type, local }) => {
               if (type === 'ImportNamespaceSpecifier') {
-                if (local.name !== 'Styled') {
-                  context.report({ node, message, fix });
+                if (local.name !== namespaceName) {
+                  errorReport();
                 }
 
                 return;
               }
 
               if (type === 'ImportSpecifier' || type === 'ImportDefaultSpecifier') {
-                context.report({ node, message, fix });
+                errorReport();
               }
             });
           }
